4692567f65c59ed558a16f07c4b92eb4bac8609a
totny <totny97@gmail.com>
2026-05-04 17:14:04 +0800

    feat: 使用https模块发送请求

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,29 @@
+import * as https from 'https';
+
 export const translate = (word: string) => {
   if (typeof word !== 'string') throw new Error('类型错误');
-  console.log('feng translate', word);
+
+  const query = new URLSearchParams({
+    q: word,
+    langpair: 'en|zh',
+  }).toString();
+
+  const options = {
+    hostname: 'api.mymemory.translated.net',
+    port: 443,
+    path: '/get?' + query,
+    method: 'GET',
+  };
+
+  const req = https.request(options, (res) => {
+    res.on('data', (d) => {
+      console.log('feng', JSON.parse(d.toString()));
+    });
+  });
+
+  req.on('error', (e) => {
+    console.error(e);
+  });
+
+  req.end();
 };
